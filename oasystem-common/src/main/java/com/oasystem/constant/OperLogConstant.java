5089205940dc44a723a1af8209e5b9a86d7aacc6
522939903@qq.com <522939903@qq.com>
2020-03-13 23:19:37 +0800

    1

--- oasystem-common/src/main/java/com/oasystem/constant/OperLogConstant.java
+++ oasystem-common/src/main/java/com/oasystem/constant/OperLogConstant.java
@@ -1,11 +1,6 @@
 package com.oasystem.constant;
 
-/**
- * @ClassName OperLogConstant
- * @Description
- * @Author suguoming
- * @Date 2020/2/8 4:30 下午
- */
+
 public class OperLogConstant {
     public static final String OPER_LOG_ADD = "1";
     public static final String OPER_LOG_UPDATE = "2";
